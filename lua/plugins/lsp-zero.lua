3c03cddb581003d1622bdd5afbc36dbdd8704fb3
Zi0LEO <leonardo.npl.02@gmail.com>
2024-10-16 09:57:23 +0200

    added snippets

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -15,13 +15,20 @@ return
   -- Autocompletion
   {
     'hrsh7th/nvim-cmp',
+    dependencies = {'L3MON4D3/LuaSnip',
+        "rafamadriz/friendly-snippets"},
     event = 'InsertEnter',
     config = function()
       local cmp = require('cmp')
+      local cmp_action = require('lsp-zero').cmp_action()
+      local cmp_format = require('lsp-zero').cmp_format({details = true})
+
+      require('luasnip.loaders.from_vscode').lazy_load()
 
       cmp.setup({
         sources = {
           {name = 'nvim_lsp'},
+          {name = 'luasnip'},
         },
         mapping = cmp.mapping.preset.insert({
           ['<C-Space>'] = cmp.mapping.complete(),
@@ -30,7 +37,7 @@ return
         }),
         snippet = {
           expand = function(args)
-            vim.snippet.expand(args.body)
+            require('luasnip').lsp_expand(args.body)
           end,
         },
       })

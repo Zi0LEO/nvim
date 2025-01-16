cc391190f9ce92def3e8e0674eb452f227780727
thecoder <leonardo.npl.02@gmail.com>
2025-01-16 11:50:30 +0100

    don't remember, probably readability

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,104 +1,77 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
 
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local border = {
-				{ "🭽", "FloatBorder" },
-				{ "▔", "FloatBorder" },
-				{ "🭾", "FloatBorder" },
-				{ "▕", "FloatBorder" },
-				{ "🭿", "FloatBorder" },
-				{ "▁", "FloatBorder" },
-				{ "🭼", "FloatBorder" },
-				{ "▏", "FloatBorder" },
-			}
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local border = {
+        { "🭽", "FloatBorder" },
+        { "▔", "FloatBorder" },
+        { "🭾", "FloatBorder" },
+        { "▕", "FloatBorder" },
+        { "🭿", "FloatBorder" },
+        { "▁", "FloatBorder" },
+        { "🭼", "FloatBorder" },
+        { "▏", "FloatBorder" },
+      }
 
-			local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+      local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 
-			function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-				opts = opts or {}
-				opts.border = opts.border or border
-				return orig_util_open_floating_preview(contents, syntax, opts, ...)
-			end
+      function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+        opts = opts or {}
+        opts.border = opts.border or border
+        return orig_util_open_floating_preview(contents, syntax, opts, ...)
+      end
 
-			vim.diagnostic.config({
-				virtual_text = true,
-				signs = true,
-				underline = true,
-				update_in_insert = true,
-				severity_sort = false,
-			})
-
-			lspconfig.lua_ls.setup({
-				settings = {
-					Lua = {
-						diagnostics = { globals = { "vim" } },
-					},
-				},
-				capabilities = capabilities,
-			})
-
-			lspconfig.gdscript.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.csharp_ls.setup({
-				capabilities = capabilities,
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "jdtls", "eslint", "html", "cssls" },
-				automatic_installation = true,
-			})
-		end,
-	},
-  {'mfussenegger/nvim-jdtls'},  
-	{
-		"neovim/nvim-lspconfig",
+      local on_attach = function(client, bufnr)
+        local opts = { buffer = bufnr, silent = true }
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      end
 
 
-		config = function()
-    local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      
-			require("lspconfig").csharp_ls.setup({
-				capabilities = capabilities,
-			})
+      vim.diagnostic.config({
+        virtual_text = {
+          prefix = "●", -- Change this symbol to your liking
+        },
+        signs = true, -- Show signs in the gutter
+        underline = true, -- Underline diagnostic text
+        update_in_insert = false, -- Update diagnostics only after leaving insert mode
+        severity_sort = false,
+      })
 
-			lspconfig.eslint.setup({
-				--- ...
-				on_attach = function(client, bufnr)
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						buffer = bufnr,
-						command = "EslintFixAll",
-					})
-				end,
-			})
-    	lspconfig.jdtls.setup({
-				capabilities = capabilities,
-			})
-			capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities.textDocument.completion.completionItem.snippetSupport = true
+      lspconfig.csharp_ls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
 
-			require("lspconfig").html.setup({
-				capabilities = capabilities,
-			})
-			require("lspconfig").cssls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.jdtls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      lspconfig.html.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      lspconfig.gdscript.setup({
+        capabilities = capabilities,
+      })
 
       lspconfig.texlab.setup({
         settings = {
@@ -114,18 +87,24 @@ return {
           },
         },
       })
-      vim.diagnostic.config({
-        virtual_text = {
-          prefix = "●", -- Change this symbol to your liking
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+          },
         },
-        signs = true, -- Show signs in the gutter
-        underline = true, -- Underline diagnostic text
-        update_in_insert = false, -- Update diagnostics only after leaving insert mode
+        capabilities = capabilities,
       })
-      
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "jdtls", "eslint", "html", "cssls" },
+        automatic_installation = true,
+      })
+    end,
+  },
+  {'mfussenegger/nvim-jdtls'},
 }

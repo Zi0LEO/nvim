45fc1767f67d4a0812860aee754faaa5d45f8335
thecoder <leonardo.npl.02@gmail.com>
2025-01-16 11:51:41 +0100

    removed duplicate and fixed vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,5 +6,6 @@ return
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "zathura"
+    vim.keymap.set("n", "<localleader>v", "<plug>(vimtex-view)", { silent = true })
   end
 }
